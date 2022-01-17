d065d5596e7fb5d8f786247d36a9ffdf5bd0a682
Dzmitry Malyshau <kvark@fastmail.com>
2022-01-17 17:53:43 -0500

    Check error and reduce the test case

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use glow::HasContext as _;
-use glutin::{event_loop::EventLoop, window::WindowBuilder, ContextBuilder};
+use glutin::{event_loop::EventLoop, window::WindowBuilder};
 
 unsafe fn check() {
-    let windowed_context_fresh = ContextBuilder::new()
+    let windowed_context_fresh = glutin::ContextBuilder::new()
+        .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGlEs, (3, 2)))
         .build_windowed(WindowBuilder::new(), &EventLoop::new())
         .unwrap();
     let windowed_context = windowed_context_fresh.make_current().unwrap();
@@ -24,6 +25,7 @@ unsafe fn check() {
     if !gl.get_program_link_status(program) {
         panic!("Link failed: {}", gl.get_program_info_log(program));
     }
+    assert_eq!(gl.get_error(), 0);
 
     println!("Active uniforms:");
     let count = gl.get_active_uniforms(program);
@@ -33,6 +35,7 @@ unsafe fn check() {
             gl.get_active_uniform(program, uniform).unwrap();
         println!("'{}' of {:?}x{}", name, utype, size);
     }
+    assert_eq!(gl.get_error(), 0);
 }
 
 fn main() {
